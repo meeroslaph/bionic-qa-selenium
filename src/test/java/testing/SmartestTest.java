2e595749479593865fd1ad08d042ea2ef7a593be
Kristina <lemonn00@gmail.com>
2014-06-20 21:19:50 +0300

    Added super-ultra-the-coolest-ever test.

---
+++ src/test/java/testing/SmartestTest.java
@@ -0,0 +1,19 @@
+package testing;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: admin
+ * Date: 20.06.14
+ * Time: 21:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class SmartestTest {
+
+    @Test
+    public void runTest() {
+        Assert.assertEquals(true, true);
+    }
+}

428a6354cc329db1d5e7b9ad9941df726ebca33a
meeroslaph <m@meeroslaph.name>
2014-09-21 22:00:10 +0300

    Updated data for Basket suite.

--- src/test/java/tests/basket/BasketData.java
+++ src/test/java/tests/basket/BasketData.java
@@ -6,8 +6,8 @@ public class BasketData {
     @DataProvider(name = "basket")
     public static Object[][] createData() {
         return new Object[][]{
-                { new String[] {"Nokia 105 Black"}, "269", "грн." },
-                { new String[] {"Samsung UE-32H6410 (UE32H6410AUXUA)"}, "7 399", "грн." }
+                { new String[] {"Nokia 105 Black"}, "319", "грн." },
+                { new String[] {"Samsung PE-43H4500 (PE43H4500AKXUA)"}, "5 999", "грн." }
         };
     }
 }

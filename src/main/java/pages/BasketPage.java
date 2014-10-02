79556cd17cc6030c237c14af64b57de9ce0703d6
meeroslaph <m@meeroslaph.name>
2014-10-02 22:46:34 +0300

    WebElement's with PageFactory pattern are used instead of By's.

--- src/main/java/pages/BasketPage.java
+++ src/main/java/pages/BasketPage.java
@@ -2,16 +2,24 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import utils.Log4Test;
 
 public class BasketPage extends BasePage {
-    private static final By title = By.className("cart-title");
-    private static final By productTitle = By.className("no-visited");
-    private static final By productPrice = By.className("cart-uah");
-    private static final By productAmount = By.className("cart-amount");
-    private static final By priceSum = By.className("cart-uah cart-sum-uah");
-    private static final By priceTotal = By.className("cart-total-uah");
+    @FindBy(className = "cart-title")
+    private WebElement title;
+    @FindBy(className = "no-visited")
+    private WebElement productTitle;
+    @FindBy(className = "cart-uah")
+    private WebElement productPrice;
+    @FindBy(className = "cart-amount")
+    private WebElement productAmount;
+    @FindBy(className = "cart-uah cart-sum-uah")
+    private WebElement priceSum;
+    @FindBy(className = "cart-total-uah")
+    private WebElement priceTotal;
 
     public BasketPage(WebDriver driver) {
         super(driver);
@@ -19,13 +27,13 @@ public class BasketPage extends BasePage {
 
     public Boolean checkProductTitle(String productName) {
         Log4Test.info("Check title of added product.");
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(productPrice));
-        return driver.findElement(productTitle).getText().equals(productName);
+        wait.until(ExpectedConditions.visibilityOf(productPrice));
+        return productTitle.getText().equals(productName);
     }
 
     public Boolean checkProductPrice(String price, String currency) {
         Log4Test.info("Check price of added product.");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(productPrice));
-        return driver.findElement(productPrice).getText().equals(price + " " + currency);
+        wait.until(ExpectedConditions.visibilityOf(productPrice));
+        return productPrice.getText().equals(price + " " + currency);
     }
 }

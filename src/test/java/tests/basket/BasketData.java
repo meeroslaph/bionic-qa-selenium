e586b9e36596dc50262671af668ef39f02d335b1
meeroslaph <m@meeroslaph.name>
2014-10-05 19:27:42 +0300

    Updated data.

--- src/test/java/tests/basket/BasketData.java
+++ src/test/java/tests/basket/BasketData.java
@@ -7,7 +7,7 @@ public class BasketData {
     public static Object[][] createData() {
         return new Object[][]{
                 { new String[] {"Nokia 105 Black"}, "319", "грн." },
-                { new String[] {"Samsung PE-43H4500 (PE43H4500AKXUA)"}, "5 999", "грн." }
+                { new String[] {"Samsung PE-43H4500 (PE43H4500AKXUA)"}, "6 899", "грн." }
         };
     }
 }
